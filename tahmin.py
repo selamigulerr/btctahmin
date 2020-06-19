62b2b91d534623921152d48269c4c3cbb1399471
selamigulerr <selami.gulerr@gmail.com>
2020-06-19 13:31:10 +0300

    Add files via upload

---
+++ tahmin.py
@@ -0,0 +1,9 @@
+import quandle
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+import sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_text_split
+
+
